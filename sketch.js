629638788af5c397d81837150814507b10979283
arya2009 <90342849+arya2009@users.noreply.github.com>
2022-01-29 10:33:14 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	var ball_options= {
+	isStatic: false, 
+	restitution:0.3, 
+	friction:0,
+	density:1.2,
+	
+	}
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ball = Matter.Bodies.circle(260,100,20,ball_options)
+	World.add(world,ball)
+	groundObj = new Ground(width/2,670,width,20)
+	leftSide = new Ground (1100,600,20,120)  
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
